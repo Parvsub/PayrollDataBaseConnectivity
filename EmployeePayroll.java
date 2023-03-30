c312c2dc5e7d874541a2ad5182b8da8661e6e187
Parvsub <Praveenboda421@gmail.com>
2023-03-30 12:05:17 +0530

    Added:New Employees are added

--- EmployeePayroll.java
+++ EmployeePayroll.java
@@ -1,6 +1,7 @@
 package com.JDBCPayroll;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.Date;
 
 public class EmployeePayroll {
     public static void main(String[] args) throws Exception{
@@ -20,6 +21,28 @@ public class EmployeePayroll {
                     Double pay = Double.parseDouble(br.readLine());
                     con.update(name,pay);
                     break;
+                case 3:
+                    System.out.println("Enter the date in format eg. '2020-01-10' ");
+                    String Date = br.readLine();
+                    con.updateDate(Date);
+                    break;
+                case 4:
+                    con.FindMultipleValues();
+                    break;
+                case 5:
+                    System.out.println("Enter the name");
+                    name = br.readLine();
+                    System.out.println("Enter the gender");
+                    String gender = br.readLine();
+                    System.out.println("Enter basic Pay");
+                    double basic = Double.parseDouble(br.readLine());
+                    System.out.println("Enter the start date eg '2010-11-02'");
+                    Date = br.readLine();
+                    System.out.println("Enter the employee phone");
+                    String phone = br.readLine();
+                    System.out.println("Enter the address");
+                    String address = br.readLine();
+                    con.addEmployee(name,gender,basic, Date,phone,address);
                 case 0:
                     con.close();
                     break;
